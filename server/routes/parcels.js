0a57dedbe2283951369caf8750a1020de49e443b
Gisele Iradukunda <giseleiradukunda@rocketmail.com>
2018-11-30 12:09:14 +0200

    [ch-db-test-162241312] endpoints test

--- server/routes/parcels.js
+++ server/routes/parcels.js
@@ -5,9 +5,12 @@ import Parcel from '../controllers/parcels';
 const router = express.Router();
 
 router.post('/', authentication(), Parcel.createParcel);
-router.get('/',authentication('admin'), Parcel.getAll);
-router.get('/:parcelId',authentication(), Parcel.getById);
-router.put('/:parcelId/destinaton',authentication('user'), Parcel.updatePropertyById);
-router.put('/:parcelId/status', authentication('admin'), Parcel.updatePropertyById);
-router.put('/:parcelId/currentLocation',authentication('admin'), Parcel.updatePropertyById);
+router.get('/', authentication('admin'), Parcel.getAll);
+router.get('/:id/:parcelId', authentication('user'), Parcel.getById);
+router.put('/:parcelId/destination', Parcel.updatePropertyById);
+
+// router.put('/:id/:parcelId/destinaton', authentication('user'), Parcel.updateDestination);
+router.put('/:parcelId/status', authentication('user'), Parcel.updatePropertyById);
+router.put('/:parcelId/currentLocation',  Parcel.updatePropertyById);
 export default router;
+
--- server/routes/users.js
+++ server/routes/users.js
@@ -5,7 +5,7 @@ import authentication from '../helper/authentication';
 const router = express.Router();
 
 
-router.get('/:id/parcels',authentication('user'), User.getUserParcels);
-// router.get('/:uid/parcels/id', User.userOneParcel);
-router.get('/', User.getAll);
+router.get('/:id/parcels', authentication('user'), User.getUserParcels);
+router.get('/:id/users', authentication('admin'), User.getAll);
+
 export default router;
